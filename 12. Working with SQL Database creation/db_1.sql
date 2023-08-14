c441be5f3ec8fe6c323af9f7ccb765b4a6e5b8c3
Dimitriy Murzin <dimitriymurzin@gmail.com>
2023-08-14 22:05:32 +0400

    Add files via upload

--- 12. Working with SQL Database creation/db_1.sql
+++ 12. Working with SQL Database creation/db_1.sql
@@ -1,48 +1,53 @@
 CREATE TABLE Genres (
-  genre_id INT PRIMARY KEY,
-  genre_name VARCHAR(255)
+  genre_id SERIAL PRIMARY KEY,
+  genre_name VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE Artists (
-  artist_id INT PRIMARY KEY,
-  artist_name VARCHAR(255)
+  artist_id SERIAL PRIMARY KEY,
+  artist_name VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE Artists_Genres (
   artist_id INT,
   genre_id INT,
+  PRIMARY KEY (artist_id, genre_id),
   FOREIGN KEY (artist_id) REFERENCES Artists(artist_id),
   FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
 );
 
 CREATE TABLE Albums (
-  album_id INT PRIMARY KEY,
-  album_name VARCHAR(255),
+  album_id SERIAL PRIMARY KEY,
+  album_name VARCHAR(60) NOT NULL,
   release_year INT
 );
 
 CREATE TABLE Artists_Albums (
   artist_id INT,
   album_id INT,
+  PRIMARY KEY (artist_id, album_id),
   FOREIGN KEY (artist_id) REFERENCES Artists(artist_id),
   FOREIGN KEY (album_id) REFERENCES Albums(album_id)
 );
 
 CREATE TABLE Tracks (
-  track_id INT PRIMARY KEY,
-  track_name VARCHAR(255),
-  duration INT
+  track_id SERIAL PRIMARY KEY,
+  track_name VARCHAR(60) NOT NULL,
+  duration INT,
+  album_id INT,
+  FOREIGN KEY (album_id) REFERENCES Albums(album_id)
 );
 
 CREATE TABLE Compilations (
-  compilation_id INT PRIMARY KEY,
-  compilation_name VARCHAR(255),
+  compilation_id SERIAL PRIMARY KEY,
+  compilation_name VARCHAR(60) NOT NULL,
   release_year INT
 );
 
 CREATE TABLE Tracks_In_Compilations (
   compilation_id INT,
   track_id INT,
+  PRIMARY KEY (compilation_id, track_id),
   FOREIGN KEY (compilation_id) REFERENCES Compilations(compilation_id),
   FOREIGN KEY (track_id) REFERENCES Tracks(track_id)
 );
