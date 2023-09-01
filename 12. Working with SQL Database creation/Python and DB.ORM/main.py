2e89ec5832f5a61c6cd49035b48aa776fa9a613b
Dimitriy Murzin <dimitriymurzin@gmail.com>
2023-09-01 23:07:32 +0400

    fix: update 01/09/2023

--- 12. Working with SQL Database creation/Python and DB.ORM/main.py
+++ 12. Working with SQL Database creation/Python and DB.ORM/main.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 from models import Publisher, Book, Stock, Shop, Sale, create_tables
 import sqlalchemy as sq
 import json
+
+load_dotenv()
 
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
@@ -18,47 +21,48 @@ create_tables(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-publisher_name = input("Введите имя издателя: ")
+def get_publisher_data(session, input_data):
+    query = session.query(
+        Book.title, Shop.name, Sale.price, Sale.date_sale
+    ).select_from(Book).join(Stock).join(Shop).join(Sale).join(Publisher)
 
-publisher = session.query(Publisher).filter(Publisher.name.like(f'%{publisher_name}%')).first()
+    if input_data.isdigit():
+        publisher_data = query.filter(Publisher.id == int(input_data)).all()
+    else:
+        publisher_data = query.filter(Publisher.name.like(f'%{input_data}%')).all()
 
-if publisher:
-    books = session.query(Book).filter(Book.id_publisher == publisher.id).all()
+    for data in publisher_data:
+        title, shop_name, price, sale_date = data
+        formatted_date = sale_date.strftime("%d-%m-%Y")
+        print(f"{title: <40} | {shop_name: <10} | {price: <8} | {formatted_date}")
 
-    for book in books:
-        sales = (
-            session.query(Book.title, Shop.name, Sale.price, Sale.date_sale)
-            .select_from(Book)
-            .join(Stock)
-            .join(Shop)
-            .join(Sale)
-            .filter(Book.id == book.id)
-            .all()
-        )
+def add_data_to_tables(session):
+    data_file_path = 'myhomework-netology/12. Working with SQL Database creation/Python and DB.ORM/fixtures/tests_data.json'
 
-        for sale in sales:
-            title, shop_name, price, sale_date = sale
-            formatted_date = sale_date.strftime("%d-%m-%Y")
-            print(f"{title} | {shop_name} | {price} | {formatted_date}")
-else:
-    print("Издатель не найден.")
+    with open(data_file_path, 'r') as fd:
+        data = json.load(fd)
 
-# data_file_path = 'myhomework-netology/12. Working with SQL Database creation/Python and DB.ORM/fixtures/tests_data.json'
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+        session.add(model(id=record.get('pk'), **record.get('fields')))
 
-# with open(data_file_path, 'r') as fd:
-#     data = json.load(fd)
+        session.commit()
+    
+if __name__ == '__main__':
+    create_tables(engine)
 
-# for record in data:
-#     model = {
-#         'publisher': Publisher,
-#         'shop': Shop,
-#         'book': Book,
-#         'stock': Stock,
-#         'sale': Sale,
-#     }[record.get('model')]
-#     print(f"Adding record: {record}")
-#     session.add(model(id=record.get('pk'), **record.get('fields')))
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-#     session.commit()
+    add_data_to_tables(session)
 
-session.close()
+    user_input = input("Введите имя или ID издателя: ")
+    get_publisher_data(session, user_input)
+
+    session.close()
--- 12. Working with SQL Database creation/Python and DB.ORM/models.py
+++ 12. Working with SQL Database creation/Python and DB.ORM/models.py
@@ -36,12 +36,12 @@ class Sale(Base):
     __tablename__ = "sale"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    # price = sq.Column(sq.Float, nullable=False)
     price = sq.Column(sq.String, nullable=False)
     date_sale = sq.Column(sq.Date, nullable=False)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
 def create_tables(engine):
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
-    # Base.metadata.drop_all(engine)
+    
