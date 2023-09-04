0c32c96423dcbf230931542c323b4c9ccc1944b4
Dimitriy Murzin <dimitriymurzin@gmail.com>
2023-09-04 22:02:47 +0400

    feat: new function Regexp 04/09/2023

--- 13. Professional work with Python/2. Regexp/main.py
+++ 13. Professional work with Python/2. Regexp/main.py
@@ -1,25 +1,66 @@
-from pprint import pprint
-import re
-from re import match
 import csv
+import re
+from pprint import pprint
 
-path_csv = '/Users/dimitriy/Developers/netology/myhomework-netology/13. Professional work with Python/2. Regexp/phonebook_raw.csv'
-with open(path_csv) as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
-# pprint(contacts_list)
+def extract_names(full_name):
+    match = re.search(r'(\w+)\s(\w+)\s?(\w+)?', full_name)
+    if match:
+        lastname = match.group(1)
+        firstname = match.group(2)
+        surname = match.group(3) if match.group(3) else ""
+    else:
+        lastname = ""
+        firstname = ""
+        surname = ""
+    return lastname, firstname, surname
 
-pattern = re.compile(r"(\+7|8)?\s?\(?(\d{3})\)?\s?-?(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(.+доб.+(\d{4}))?")
+def format_phone(phone):
+    match = re.search(r'(\+7|8)?\s?\(?(\d{3})\)?\s?-?(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(.+доб.+(\d{4}))?', phone)
+    if match:
+        phone = f'+7({match.group(2)}){match.group(3)}-{match.group(4)}-{match.group(5)}'
+        if match.group(7):
+            phone += f' доб.{match.group(7)}'
+    return phone
 
-filtred_values = [item for sublist in contacts_list for item in sublist if pattern.match(item)]
+def process_contact(contact):
+    full_name = ' '.join(contact[:3])
+    lastname, firstname, surname = extract_names(full_name)
+    phone = format_phone(contact[5])
+    return lastname, firstname, surname, phone
 
-for values in filtred_values:
-  pprint(values)
+def process_csv(input_filename, output_filename):
+    with open(input_filename, encoding='utf-8') as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
 
-## 2. Сохраните получившиеся данные в другой файл.
-## Код для записи файла в формате CSV
-with open("phonebook.csv", "w") as f:
-  datawriter = csv.writer(f, delimiter=',')
+    processed_contacts_list = [['lastname', 'firstname', 'surname', 'phone']]
 
-## Вместо contacts_list подставьте свой список:
-  datawriter.writerows(contacts_list)
+    for contact in contacts_list[1:]:
+        lastname, firstname, surname, phone = process_contact(contact)
+
+        is_copy = False
+        for i, processed_contact in enumerate(processed_contacts_list):
+            if processed_contact[0] == lastname and processed_contact[1] == firstname:
+                is_copy = True
+                if len(surname) > len(processed_contact[2]):
+                    processed_contact[2] = surname
+                if len(phone) > len(processed_contact[3]):
+                    processed_contact[3] = phone
+
+        if not is_copy:
+            processed_contacts_list.append([lastname, firstname, surname, phone])
+
+    with open(output_filename, 'w', encoding='utf-8', newline='') as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerows(processed_contacts_list)
+
+    return processed_contacts_list
+
+if __name__ == "__main__":
+    input_filename = 'myhomework-netology/13. Professional work with Python/2. Regexp/phonebook_raw.csv'
+    output_filename = 'myhomework-netology/13. Professional work with Python/2. Regexp/phonebook.csv'
+
+    processed_contacts = process_csv(input_filename, output_filename)
+
+    for contact in processed_contacts:
+        print("Фамилия:", contact[0], "Имя:", contact[1], "Отчество:", contact[2], "Телефон:", contact[3])
